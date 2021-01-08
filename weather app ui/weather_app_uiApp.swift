e7a1debeda07b151851e93e971601645feee553d
pradyun <pradyun.setti@gmail.com>
2021-01-09 12:42:59 +1300

    Initial Commit

---
+++ weather app ui/weather_app_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_uiApp.swift
+//  weather app ui
+//
+//  Created by Pradyun Setti on 9/01/21.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
